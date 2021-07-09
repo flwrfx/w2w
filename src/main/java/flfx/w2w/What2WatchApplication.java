d4de195dc173d8dbaac12a329ef5e1dee5ea2241
flwrfx <briceflowers@hotmail.com>
2021-07-09 18:46:51 -0500

    Initial commit

---
+++ src/main/java/flfx/w2w/What2WatchApplication.java
@@ -0,0 +1,13 @@
+package flfx.w2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What2WatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(What2WatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flfx/w2w/What2WatchApplicationTests.java
@@ -0,0 +1,13 @@
+package flfx.w2w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What2WatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
